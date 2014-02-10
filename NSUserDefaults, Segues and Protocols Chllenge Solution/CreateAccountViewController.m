5bc657b5c9d80bd01bad3f901809b494a43458d0
rochec <rochec@gmail.com>
2014-02-09 19:17:29 -0600

    first commit

---
+++ NSUserDefaults, Segues and Protocols Chllenge Solution/CreateAccountViewController.m
@@ -0,0 +1,38 @@
+//
+//  CreateAccountViewController.m
+//  NSUserDefaults, Segues and Protocols Chllenge Solution
+//
+//  Created by Chris Roche on 2/9/14.
+//  Copyright (c) 2014 Chris Roche. All rights reserved.
+//
+
+#import "CreateAccountViewController.h"
+
+@interface CreateAccountViewController ()
+
+@end
+
+@implementation CreateAccountViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ NSUserDefaults, Segues and Protocols Chllenge Solution/SignInViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignInViewController.h
+//  NSUserDefaults, Segues and Protocols Chllenge Solution
+//
+//  Created by Chris Roche on 2/9/14.
+//  Copyright (c) 2014 Chris Roche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignInViewController : UIViewController
+
+@end
